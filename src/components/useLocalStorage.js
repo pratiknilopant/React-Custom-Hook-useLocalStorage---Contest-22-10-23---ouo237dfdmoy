9f56f35be62063a3d11aa492996f273b5bfcfa45
pratiknilopant <mr.pratiknilopant@gmail.com>
2023-10-22 10:22:40 +0530

    Final Commit

--- src/components/useLocalStorage.js
+++ src/components/useLocalStorage.js
@@ -2,7 +2,15 @@ import { useEffect } from "react";
 import { useState } from "react";
 
 const useLocalStorage = (stateVar, value) => {
-  
+  const storedValue = localStorage.getItem(stateVar);
+
+  const [finalvalue, setFinalvalue] = useState(storedValue ? JSON.parse(storedValue) : value);
+
+  useEffect(() => {
+    localStorage.setItem(stateVar, JSON.stringify(finalvalue));
+  }, [stateVar, finalvalue]);
+
+  return [finalvalue, setFinalvalue];
 };
 
 export default useLocalStorage;
